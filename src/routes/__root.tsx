2164b6fc286f2ade47c3143f9c61289a047b201d
Brooke Hatton <10200358+Brookke@users.noreply.github.com>
2026-03-08 21:47:24 +0000

    add biome and run it

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,46 +1,47 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
 import {
-  MantineProvider,
-  AppShell,
-  ColorSchemeScript,
-  Text,
-  Anchor,
-  Group,
+	Anchor,
+	AppShell,
+	ColorSchemeScript,
+	Group,
+	MantineProvider,
+	Stack,
+	Text,
 } from "@mantine/core";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import "@mantine/core/styles.css";
 import { mantineTheme } from "../theme";
 
 export const Route = createRootRoute({
-  component: RootComponent,
+	component: RootComponent,
 });
 
 function RootComponent() {
-  return (
-    <>
-      <ColorSchemeScript defaultColorScheme="auto" />
-      <MantineProvider theme={mantineTheme} defaultColorScheme="auto">
-        <AppShell>
-          <AppShell.Main>
-            <Outlet />
-            <Group justify="center" p="xl">
-              <Text>
-                Made with{" "}
-                <span aria-label="love" role="img">
-                  💖
-                </span>{" "}
-                by{" "}
-                <Anchor
-                  href="https://brookehatton.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Brooke
-                </Anchor>
-              </Text>
-            </Group>
-          </AppShell.Main>
-        </AppShell>
-      </MantineProvider>
-    </>
-  );
+	return (
+		<>
+			<ColorSchemeScript defaultColorScheme="auto" />
+			<MantineProvider theme={mantineTheme} defaultColorScheme="auto">
+				<AppShell>
+					<Stack component={AppShell.Main} mih="100dvh" justify="space-between">
+						<Outlet />
+						<Group justify="center" p="xl">
+							<Text>
+								Made with{" "}
+								<span aria-label="love" role="img">
+									💖
+								</span>{" "}
+								by{" "}
+								<Anchor
+									href="https://brookehatton.com"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Brooke
+								</Anchor>
+							</Text>
+						</Group>
+					</Stack>
+				</AppShell>
+			</MantineProvider>
+		</>
+	);
 }
